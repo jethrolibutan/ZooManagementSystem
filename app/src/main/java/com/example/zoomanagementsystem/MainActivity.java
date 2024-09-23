a46d484c9a9f898ce662a88fa29ca4a5bf17ce05
jethrolibutan <jethrolibutan@gmail.com>
2024-09-23 18:54:56 -0400

    Cleaning up unused code in Main Activity, fixing the sounds and names.
    Also cleaned up the activity_main.xml layout.

--- app/src/main/java/com/example/zoomanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/zoomanagementsystem/MainActivity.java
@@ -7,11 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import com.example.zoomanagementsystem.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textViewAnimalName;
     private TextView textViewAnimalDetails;
 
     @Override
@@ -19,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textViewAnimalName = findViewById(R.id.textViewAnimalName);
         textViewAnimalDetails = findViewById(R.id.textViewAnimalDetails);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -33,10 +30,10 @@ public class MainActivity extends AppCompatActivity {
         Animal animal;
         switch (view.getId()) {
             case R.id.buttonLion:
-                animal = new Lion("Simba", "Carnivore");
+                animal = new Lion("Simba", "Lion");
                 break;
             case R.id.buttonElephant:
-                animal = new Elephant("Dumbo", "Herbivore");
+                animal = new Elephant("Dumbo", "Elephant");
                 break;
             default:
                 return;
@@ -45,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void displayAnimalDetails(Animal animal) {
-        textViewAnimalName.setText(animal.name);
         textViewAnimalDetails.setText(animal.displayDetails());
     }
 }
